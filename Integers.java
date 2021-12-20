c0882730bda3261dfda59d147d7366626e08a3e1
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-20 21:48:52 +0530

    int programme

---
+++ Integers.java
@@ -0,0 +1,15 @@
+class Integer{
+     static int []integers = {2,3,53,6,79,85,3,7,4,9};
+     public static void main(String[] args){
+     System.out.println(integers[0]);
+     System.out.println(integers[1]);
+     System.out.println(integers[2]);
+     System.out.println(integers[3]);
+     System.out.println(integers[4]);
+     System.out.println(integers[5]);
+     System.out.println(integers[6]);
+     System.out.println(integers[7]);
+     System.out.println(integers[8]);
+     System.out.println(integers[9]);
+     }
+}
